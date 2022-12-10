881beaf1ca58293d8c64f5ac619bf2f9c48920aa
ThaneCawdor <gavacat@icloud.com>
2022-12-10 22:03:35 +0300

    Remove files

--- sandbox/src/test/java/ru/stqa/pft/sanbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sanbox/PointTest.java
@@ -7,8 +7,11 @@ public class PointTest {
     @Test
     public void testPoint(){
         Point p1 = new Point(3.0,3.0);
-        Point p2 = new Point(2.5,10.5);
+        Point p2 = new Point(5.0,5.0);
+        Point p3 = new Point(2.5,10.5);
 
-        Assert.assertEquals(Math.round(p1.distance(p2)),8.0);
+        Assert.assertEquals(Math.round(p1.distance(p2)),3.0);
+        Assert.assertEquals(Math.round(p1.distance(p3)),8.0);
+        Assert.assertEquals(Math.round(p2.distance(p3)),6.0);
     }
 }
