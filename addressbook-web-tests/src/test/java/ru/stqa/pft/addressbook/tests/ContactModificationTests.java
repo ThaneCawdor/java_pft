38348c4f0040a74a48a26a60f92bca5405096a2a
ThaneCawdor <gavacat@icloud.com>
2023-01-11 20:02:30 +0300

    add 5.8 Hash

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -26,8 +26,8 @@ public class ContactModificationTests extends TestBase {
         ContactData contact = new ContactData()
                 .withId(modifyContact.getId()).withFirstName("Max").withLastName("Payne").withAddress("Brooklyn").withPhones("+17184848122").withEmail("www.valorservice.com");
         app.contact().modify(contact);
+        assertThat(app.contact().count(), equalTo(before.size()));
         Contacts after = app.contact().all();
-        assertEquals(after.size(), before.size());
         assertThat(after, equalTo(before.withOut(modifyContact).withAdded(contact)));
     }
 }
