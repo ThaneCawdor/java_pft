980337eccce241cf9a4368c2f3d05513fffcdf08
ThaneCawdor <gavacat@icloud.com>
2023-01-17 13:49:52 +0300

    add 6.4 Parametrization

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,21 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-    @Test
-    public void testContactCreation() {
+
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        File photo = new File("src/test/resources/max.jpg");
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new ContactData().withFirstName("Max").withLastName("Payne").withAddress("Brooklyn")
+                .withAllPhones("+17184848122").withAllEmail("www.valorservice.com").withPhoto(photo)});
+        list.add(new Object[]{new ContactData().withFirstName("Max1").withLastName("Payne1").withAddress("Brooklyn1")
+                .withAllPhones("+17184848122").withAllEmail("www.valorservice.com").withPhoto(photo)});
+        list.add(new Object[]{new ContactData().withFirstName("Max2").withLastName("Payne2").withAddress("Brooklyn2")
+                .withAllPhones("+17184848122").withAllEmail("www.valorservice.com").withPhoto(photo)});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.goTo().contactPage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/max.jpg");
-        ContactData contact = new ContactData().withFirstName("Max").withLastName("Payne").withAddress("Brooklyn").withPhoto(photo).withAllPhones("+17184848122").withAllEmail("www.valorservice.com");
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
